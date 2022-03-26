18e3c1cfec854489c4354cc59ca2e326359cc723
Matthieu <matthieu.labaune@gmail.com>
2022-03-26 15:19:16 +0100

    ADD Sanctum config Authentication by token

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,25 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+  <div>
+ {{ user }}
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+export default {
+  data() {
+    return {
+      user: null
     }
+  },
+  methods: {
+    async getUser() {
+      let response = await axios.get('/api/user')
+
+      this.user = response.data
+    }
+  },
+  mounted() {
+    this.getUser()
+  }
+}
 </script>
